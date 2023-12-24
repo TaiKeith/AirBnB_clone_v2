230bfb0cfabb2c90c181ae0de5ef7c8bc6a6673a
Tai Keith <keithsteve.ks@hotmail.com>
2023-12-24 03:25:44 -0800

    Fixed code

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -9,7 +9,7 @@ echo "<html>
   <head>
   </head>
   <body>
-    Hello World!
+    Holberton School
   </body>
 </html>" | sudo tee /data/web_static/releases/test/index.html
 
@@ -18,6 +18,6 @@ sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 # Give ownership of /dada/ folder to ubuntu user and group
 sudo chown -R ubuntu:ubuntu /data/
 
-sed -i '/server {/a \\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}' /etc/nginx/sites-available/default
+sed -i '/server {/a \\n    location /hbnb_static {\n\talias /data/web_static/current/;\n    }\n' /etc/nginx/sites-available/default
 
 service nginx restart

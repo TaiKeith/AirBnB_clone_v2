188d2f9c4664c4b0bafafef4233c8b15abff059c
Tai Keith <keithsteve.ks@hotmail.com>
2024-01-03 11:23:59 -0800

    Update

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 # This script sets up my web servers for the deployment of web_static
-apt-get update
-apt-get install -y nginx
+sudo apt-get update
+sudo apt-get install -y nginx
 
+sudo mkdir -p /data/web_static/shared/
 sudo mkdir -p /data/web_static/releases/test/
-sudo mkdir -p /data/web_static/shared
 
 echo "<html>
   <head>
@@ -12,13 +12,13 @@ echo "<html>
   <body>
     Holberton School
   </body>
-</html>" | sudo tee /data/web_static/releases/test/index.html
+</html>" >> /data/web_static/releases/test/index.html
 
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 
-# Give ownership of /dada/ folder to ubuntu user and group
+# Give ownership of /data/ folder to ubuntu user and group
 sudo chown -R ubuntu:ubuntu /data/
 
-sed -i '/server {/a \\n    location /hbnb_static {\n\talias /data/web_static/current/;\n    }\n' /etc/nginx/sites-available/default
+sudo sed -i '/server {/a \\n    location /hbnb_static {\n\talias /data/web_static/current/;\n    }\n' /etc/nginx/sites-available/default
 
 service nginx restart

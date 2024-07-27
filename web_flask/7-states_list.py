43cf7f9015df92fe205dc8a18590c0620cee7815
Tai Keith <keithsteve.ks@hotmail.com>
2024-07-27 06:36:31 -0700

    Fixed pycodestyle compliance

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -9,17 +9,17 @@ from models.state import State
 app = Flask(__name__)
 
 
+@app.teardown_appcontext
+def teardown(self):
+    """A method that removes the current SQLAlchemy session"""
+    storage.close()
+
+
 @app.route("/states_list", strict_slashes=False)
 def states_list():
     """Displays a HTML page with a list of all states"""
     states = storage.all(State)
     return render_template('7-states_list.html', states=states)
-
-
-@app.teardown_appcontext
-def teardown(self):
-    """A method that removes the current SQLAlchemy session"""
-    storage.close()
 
 
 if __name__ == "__main__":
